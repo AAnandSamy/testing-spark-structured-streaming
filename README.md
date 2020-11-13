654bab7acea264df381d6815552c8ddab6d8c09b
Ananda Ayyasamy <ananda.ayyasamy@aciworldwide.com>
2020-11-12 21:55:03 -0500

    doc updates

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# testing-spark-structured-streaming
+# Testing spark structured streaming
+Unit Testing Apache Spark Structured Streaming using MemoryStream.
+Unit testing Apache Spark Structured Streaming jobs using MemoryStream in a non-trivial task.In this post, therefore, I will show you how to start writing unit tests of Spark Structured Streaming.
+
+###MemoryStream
+MemoryStream is one of the streaming sources available in Apache Spark. This source allows us to add and store data in memory, which is very convenient for unit testing
+This saves us a ton of time over running locally embedded Kafka.
+
+We then create a streaming query that writes to a MemorySink. This allows us to test an end to end streaming query, without the need to Mock out the source and sink in our structured streaming application. This means you can plug in the tried and true DataSources from Spark and focus instead on ensuring that your application code is running hiccup free.
+
+###Writing Spark Structured Streaming job
+
+I am using a stringified JSON, that contains information I am normally receiving from my Kafka data source. You should use any data that fits your use case.
+
+Application Configuration 
+- config the sample events(app-config)
+- provide checkpointing location
+```
+   value={"Organisation_Name":"City of York Council","Directorate":"Economy and Place","Department":"Transp Highways & Environment","Service_Plan":"Transport","Creditor_Name":"Road Safety Analysis Ltd","Payment_Date":"2/4/2020 0:00","Card_Transaction":"","Transaction_No":"202021CR00000001","Net_Amount":995,"Irrecoverable_VAT":"","Subjective_Group":"Supplies And Services","Subjective_Subgroup":"Grants and Subscriptions","Subjective_Detail":"Subscriptions"}   
+   checkpointLocation=/data/checkpoint/real-time/payments
+```
+    
--- src/main/scala/com/org/data/stream/test/DataStreamTester.scala
+++ src/main/scala/com/org/data/stream/test/DataStreamTester.scala
@@ -4,49 +4,55 @@ import java.io.FileInputStream
 import java.util.Properties
 
 import org.apache.spark.sql.execution.streaming.MemoryStream
-import org.apache.spark.sql.{SQLContext, SparkSession}
+import org.apache.spark.sql.{DataFrame, Dataset, SQLContext, SparkSession}
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.streaming.OutputMode
 
-object DataStreamTester {
+object DataStreamTester extends App {
 
-  def main(args: Array[String]): Unit = {
+  // loading app config
+  val prop = new Properties()
+  prop.load(new FileInputStream("app-config.properties"))
+  // app config setu
+  val sampleDta = prop.getProperty("value")
+  val checkpointLocation = prop.getProperty("checkpointLocation")
 
-    val prop = new Properties()
-    prop.load(new FileInputStream("app-config.properties"))
+  val spark = SparkSession
+    .builder()
+    .appName("Welcome to Spark 2.+++")
+    .master("local[*]")
+    .getOrCreate();
 
-    val sampleDta = prop.getProperty("value")
-    val checkpointLocation = prop.getProperty("checkpointLocation")
+  import spark.implicits._
 
-    val spark = SparkSession
-      .builder()
-      .appName("Welcome to Spark 2.+++")
-      .master("local[*]")
-      .getOrCreate();
+  implicit val ctx: SQLContext = spark.sqlContext
+  // prepare schema based on given message
+  val ds = spark.createDataset(s"""${sampleDta}""" :: Nil)
+  val ds_schema = spark.read.json(ds).schema
 
-    import spark.implicits._
-    implicit val ctx: SQLContext = spark.sqlContext
-
-    val ds = spark.createDataset(s"""${sampleDta}""" :: Nil)
-    val ds_schema = spark.read.json(ds).schema
-
+  def readFromKafka(dta: String): Dataset[String] = {
     val stream = MemoryStream[String]
-    stream.addData(sampleDta)
+    stream.addData(dta)
     val sDs = stream.toDS()
-
-    val transDf = sDs.select(from_json($"value",ds_schema) as "dta")
-      .select("dta.*")
-
-    transDf
-      .writeStream
-      .format("console")
-      .queryName("datastream")
-      .outputMode(OutputMode.Append())
-      .option("checkpointLocation", checkpointLocation)
-      .start()
-      .processAllAvailable()
-
+    sDs
   }
+
+  def transform(ds: Dataset[String]): DataFrame = {
+    ds.select(from_json($"value", ds_schema) as "dta")
+      .select("dta.*")
+  }
+
+  val kds = readFromKafka(sampleDta)
+  val transDf = transform(kds)
+
+  transDf
+    .writeStream
+    .format("console")
+    .queryName("datastream")
+    .outputMode(OutputMode.Append())
+    .option("checkpointLocation", checkpointLocation)
+    .start()
+    .processAllAvailable()
 
 
 }
